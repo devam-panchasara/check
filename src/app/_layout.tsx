91e87789248f6b03d5a8442c54226f4120253be1
devam-panchasara <devam.p@brucira.com>
2026-07-09 16:40:48 +0530

    chore: update ruttl-mobile-sdk reference to version 1.0.2 in package.json and yarn.lock

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,6 +1,6 @@
+import { BugTrackingRoot } from '@ruttl/mobile-sdk';
 import { DarkTheme, DefaultTheme, ThemeProvider } from 'expo-router';
 import { useColorScheme } from 'react-native';
-import { BugTrackingRoot } from '@ruttl/mobile-sdk';
 
 import { AnimatedSplashOverlay } from '@/components/animated-icon';
 import AppTabs from '@/components/app-tabs';
@@ -8,10 +8,15 @@ import AppTabs from '@/components/app-tabs';
 export default function TabLayout() {
   const colorScheme = useColorScheme();
   return (
+    //live projects
     <BugTrackingRoot
       projectID="A8WDdHuCZhJCIrifxHWd"
       token="BD7KAqd0O2hhJcSkoF8N8pBZaKS2"
     >
+      {/* <BugTrackingRoot
+      projectID="mfeq3gWkQEwR6wdOjIop"
+      token="6rAzmpUS2sRTWWK9RtFJ8XR2Mes2"
+    > */}
       <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
         <AnimatedSplashOverlay />
         <AppTabs />
